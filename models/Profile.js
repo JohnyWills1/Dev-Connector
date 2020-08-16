9acd891a9ebe6f018473cdcad0e8c50ba446a562
Johny Wills <jonathan.wills123456@gmail.com>
2020-08-16 01:22:45 +0100

    fixed github repo redundancy

--- models/Profile.js
+++ models/Profile.js
@@ -56,37 +56,37 @@ const ProfileSchema = new mongoose.Schema({
             type: String
           }
         }
-      ],
-      education: [
-        {
-          school: {
-            type: String,
-            required: true
-          },
-          degree: {
-            type: String,
-            required: true
-          },
-          fieldofstudy: {
-            type: String,
-            required: true
-          },
-          from: {
-            type: Date,
-            required: true
-          },
-          to: {
-            type: Date
-          },
-          current: {
-            type: Boolean,
-            default: false
-          },
-          description: {
-            type: String
-          }
+    ],
+    education: [
+      {
+        school: {
+          type: String,
+          required: true
+        },
+        degree: {
+          type: String,
+          required: true
+        },
+        fieldofstudy: {
+          type: String,
+          required: true
+        },
+        from: {
+          type: Date,
+          required: true
+        },
+        to: {
+          type: Date
+        },
+        current: {
+          type: Boolean,
+          default: false
+        },
+        description: {
+          type: String
         }
-      ],
+      }
+    ],
       social: {
         youtube: {
           type: String
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
-const request = require('request');
+const axios = require('axios');
 const config = require('config');
 
 const Profile = require('../../models/Profile');
@@ -301,34 +301,26 @@ router.delete('/education/:exp_id', auth, async (req, res) => {
     }
 });
 
-// @route  GET api/profile/github/:username
-// @desc   Get user repos from GitHub
-// @access Public
-router.get('/github/:username', (req, res) => {
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
     try {
-        const options = {
-            uri: `https://api.github.com/users/
-            ${req.params.username}/repos?per_page=5&sort=created:asc
-            &client_id=${config.get('githubClientId')}
-            &client_secret=${config.get('githubSecret')}`,
-            method: 'GET',
-            headers: { 'user-agent': 'node.js' }
-        };
-
-        request(options, (error, response, body) => {
-            if(error) console.error(error);
-            
-            if(response.statusCode !== 200){
-                res.status(404).json({ msg:'No GitHub Profile Found.' });
-            }
-
-            res.json(JSON.parse(body));
-        });
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
     }
-});
+  });
 
 
 module.exports = router;
